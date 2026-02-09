7b7e19e7a731489ef452164eacb016732a606921
Ramazan <rambodizma972n@gmail.com>
2026-02-09 16:06:29 +0300

    Changes Url

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,3 +1,3 @@
-export const api_base_url = "http://192.168.1.181:8080/v1/api";
-//export const api_base_url = process.env.API_BASE_URL || "https://api.tekera21.com/v1/api";
-export const user_api_base_url = process.env.USER_API_BASE_URL || "http://192.168.1.175:8080";
+//export const api_base_url = "http://192.168.1.181:8080/v1/api";
+export const api_base_url = process.env.API_BASE_URL || "https://api.tekera21.com/v1/api";
+export const user_api_base_url = process.env.USER_API_BASE_URL || "http://37.148.214.204:8088";
